7c21697825ae9da1758a880fe56a4fb25193ca6d
DeltaBiba <bibaievdanylo@gmail.com>
2025-04-06 13:54:10 +0200

    Setting styles on StartScreen

--- frontend/src/components/StartScreen/StartScreen.jsx
+++ frontend/src/components/StartScreen/StartScreen.jsx
@@ -1,18 +1,15 @@
-import styles from "./StartScreen.module.css"
+import styles from "./StartScreen.module.css";
 export const StartScreen = () => {
     return (
-        <><div className={styles.container}>
-            <div className={styles.greetings}>
-                <h2>Hello</h2>
-                <button>
-                    Get started
-                </button>
+        <div className={styles.container}>
+            <div className={styles.greeting}>
+                <h2>Welcome to our project</h2>
+                <button className={styles.button}>Get started</button>
             </div>
             <div className={styles.sign}>
-            <h2>Devchat</h2>
-            <h3>Illia Buliekha, Danylo Bibaiev</h3>
+                <h2>Devchat</h2>
+                <h3>Illia Buliekha, Danylo Bibaiev</h3>
             </div>
         </div>
-        </>
-    )
-}
+    );
+};
